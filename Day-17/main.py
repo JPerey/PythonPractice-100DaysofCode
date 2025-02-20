b0afcf6484a6aca547394b5d7b3bb59193a27abc
Jam Perey <jamjam.perey@gmail.com>
2025-02-20 06:11:28 -0800

    Day 17 - creating constructor pt1

---
+++ Day-17/main.py
@@ -0,0 +1,7 @@
+class User: 
+    def __init__(self, hobby, job, user_id):
+        self.hobby = hobby
+        self.job = job
+        self.user_id = user_id
+
+tim = User("Guitar", "Software Engineer", "001")
