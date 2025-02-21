f223a0f1066be6f8f5643b1aa468138918587598
Jam Perey <jamjam.perey@gmail.com>
2025-02-21 06:44:08 -0800

    Add Quiz Game Start files

--- Day-17/main.py
+++ Day-17/main.py
@@ -3,5 +3,18 @@ class User:
         self.hobby = hobby
         self.job = job
         self.user_id = user_id
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
 
 tim = User("Guitar", "Software Engineer", "001")
+Brenda = User("Piano", "Data Scientist", "002")
+
+tim.follow(Brenda)
+
+print(f"tim followers: {tim.followers} || tim following: {tim.following}")
+print(f"Brenda followers: {Brenda.followers} || Brenda following: {Brenda.following}")
